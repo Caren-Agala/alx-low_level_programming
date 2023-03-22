deeb2d58576b3def397c11383485c02677f3ef0e
Caren-Agala <carenagala324@gmail.com>
2023-03-22 16:29:54 -0700

    mandatory tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the sum
+ * @a: first integer
+ * @b: second integer
+ * Return: returns the sum
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
